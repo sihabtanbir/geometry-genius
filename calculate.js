99ee66c0d1de107d61422080f431b67a205ec9dd
Tanbir Ahmmed Sihab <sihab.cse@gmail.com>
2024-06-22 17:57:42 +0600

    module 26.3

--- calculate.js
+++ calculate.js
@@ -2,5 +2,12 @@
 // triangle area 
 
 function calculateTriangle(){
-    alert('helo')
+    const trinangleBaseText = document.getElementById('triangleBase').value ;
+    const trinangleBase = parseFloat(trinangleBaseText)
+    
+    const trinangleHeightText = document.getElementById('triangleHeight').value ;
+    const trinangleHeight = parseFloat(trinangleBaseText);
+    const triangle = 0.5* trinangleBase * trinangleHeight;
+
+    alert(triangle)
 }
