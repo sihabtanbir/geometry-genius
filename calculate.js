1a7b3c0f010aa86cac5de929b407c15d9c596ce6
Tanbir Ahmmed Sihab <sihab.cse@gmail.com>
2024-06-24 01:34:14 +0600

    reusable function

--- calculate.js
+++ calculate.js
@@ -6,7 +6,7 @@ function calculateTriangle(){
     const trinangleBase = parseFloat(trinangleBaseText)
     
     const trinangleHeightText = document.getElementById('triangleHeight').value ;
-    const trinangleHeight = parseFloat(trinangleBaseText);
+    const trinangleHeight = parseFloat(trinangleHeightText);
     const triangle = 0.5* trinangleBase * trinangleHeight;
 
     alert(triangle)
@@ -23,4 +23,18 @@ function rectangleCalculate(){
     const rectangle =  rectangleWidth * rectangleLength;
 
     alert(rectangle)
+}
+
+function getInput(inputFildId){
+    const inputFild = document.getElementById(inputFildId).value;
+    const inputValue = parseFloat(inputFild);
+    return inputValue;
+}
+
+function parallelogramCalculate(){
+    const base = getInput('parallelogramBase');
+    
+    const height = getInput('parallelogramHeight');
+    const result = base * height;
+    alert(result);
 }
